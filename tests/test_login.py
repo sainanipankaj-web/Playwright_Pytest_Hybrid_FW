101fa16ec54c110187e0e3e51c69ad5d67a9a6e4
pankaj.sainani <PSAI15@safeway.com>
2026-05-14 13:19:50 +0530

    Added logic to save the log_in_session and using it to run other test without Login In to system using existing login_state

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,11 +1,13 @@
-from playwright.sync_api import Page
+from playwright.sync_api import (Browser, Page,
+expect)   
+
 
 
 def test_valid_login(page: Page):
 
     # Open application
     page.goto("https://www.saucedemo.com")
-
+    
     # Enter username
     page.locator("#user-name").fill("standard_user")
 
@@ -16,6 +18,27 @@ def test_valid_login(page: Page):
     page.locator("#login-button").click()
 
     # Assertion
-    assert page.locator(".title").text_content() == "Products"
+    expect(page.locator(".title")).to_have_text("Products")
 
+     # save after login
+    page.context.storage_state(
+        path="login_state.json"
+    )
+
+    print("Session saved successfully")
+
+    print("Login successful")
+
+
+def test_login_with_session_info(browser: Browser):
+    context = browser.new_context(storage_state="login_state.json")
+    page = context.new_page()
+
+    # Open application
+    page.goto("https://www.saucedemo.com/inventory.html")
+
+    # Assertion
+    expect(page.locator(".title")).to_have_text("Products")
+
+    context.close()
     print("Login successful")
